65d4a975b62e49ddf8e4247aa76b4400cd8dd0b0
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-04-06 10:53:13 +0600

    THis is very basic syntax of class and object in oop in py (updated)

--- oop_01.py
+++ oop_01.py
@@ -1,15 +1,17 @@
 class Student:
-    def set_name(self, name): #This is method or function
+    def set_name(self, name, roll): #This is method or function
         self.name = name
+        self.roll = roll
         
     def get_name(self): #This is method or function
-        return self.name
+        return self.name, self.roll
     
 student1 = Student() # Here student1 is an object
-student1.set_name("Mohd. Imran Hossain")
+student1.set_name("Mohd. Imran Hossain", 12)
 
 # We can print now the student name
 print(student1.name)
+print(student1.roll)
 
 # we can print student name another way
 print(student1.get_name())
